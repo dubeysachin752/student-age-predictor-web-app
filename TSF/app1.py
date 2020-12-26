7177c747fb5f65f1c4c2b937d70d1df6d2e7581c
Sachin dubey <67370132+dubeysachin752@users.noreply.github.com>
2020-12-27 02:20:22 +0530

    Update app1.py

--- TSF/app1.py
+++ TSF/app1.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jul 15 23:27:40 2020
-
-@author: Harsha
-"""
 
 import numpy as np
 from flask import Flask, request, jsonify, render_template
